0cab25b61d069f87fee8711ddc3bfb453d3cc18f
Rachel Ehrlich <Rachel@Rachels-MacBook-Pro.local>
2015-09-29 15:55:12 -0400

    add function to parse lines

--- load.py
+++ load.py
@@ -5,12 +5,23 @@ Created September 28, 2015
 '''
 # import urllib2
 from py2neo import cypher, Graph
+import re
+
+def parse_line(line):
+    pattern = re.compile('"(.*)","(.*)\s(\d+)"')
+    matches = pattern.match(line)
+    if matches is None:
+        return
+    character = matches.groups()[0]
+    comic = matches.groups()[1]
+    issue = matches.groups()[2]
+    print character + " appears in " + comic + " #" + str(issue)
 
 def read_character_issues(db):
     # data = urllib2.urlopen('http://exposedata.com/marvel/data/source.csv')
     data = open('bin/character_issue.csv', 'r')
     for line in data:
-        print line
+        parse_line(line)
 
 db = Graph()
 read_character_issues(db)
