297155dd90c7af229785d4e0b883658577f4768b
Rachel Ehrlich <rae84@cornell.edu>
2015-09-30 14:59:39 -0400

    add file to generate edge list for gephi

---
+++ print.py
@@ -0,0 +1,16 @@
+'''
+Created September 30, 2015
+
+@Author Rachel Ehrlich
+'''
+from py2neo import Graph, Relationship
+
+def write_knows_edge_list_for_gephi(graph):
+    fout = open('bin/knows_edge_list_gephi.csv', 'w')
+    for rel in graph.match(rel_type="KNOWS"):
+        edge_str = "{0},{1},{2}\r\n".format(rel.nodes[0]["name"], rel.nodes[1]["name"], rel["common_issues"])
+        fout.write(edge_str)
+    fout.close()
+
+graph = Graph()
+write_knows_edge_list_for_gephi(graph)
