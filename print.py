1670056074a2e498dd9fd680aae1b606f274f9eb
Rachel Ehrlich <rae84@cornell.edu>
2015-09-30 15:11:11 -0400

    change edge file to be separated with ; instead of ,

--- print.py
+++ print.py
@@ -8,7 +8,7 @@ from py2neo import Graph, Relationship
 def write_knows_edge_list_for_gephi(graph):
     fout = open('bin/knows_edge_list_gephi.csv', 'w')
     for rel in graph.match(rel_type="KNOWS"):
-        edge_str = "{0},{1},{2}\r\n".format(rel.nodes[0]["name"], rel.nodes[1]["name"], rel["common_issues"])
+        edge_str = "{0};{1};{2}\r\n".format(rel.nodes[0]["name"], rel.nodes[1]["name"], rel["common_issues"])
         fout.write(edge_str)
     fout.close()
 
